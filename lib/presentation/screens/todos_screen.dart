eae34ac34d46e97a018b130d40b39a602d7eac39
Danushan Ravendran <it20051648@my.sliit.lk>
2022-03-23 15:26:05 +0530

    screens

---
+++ lib/presentation/screens/todos_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Todo_Screan extends StatelessWidget {
+  const Todo_Screan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text("Todo screen"),
+    ));
+  }
+}
